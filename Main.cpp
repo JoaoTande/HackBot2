80207e2dfee7dcc9c4954f106f40f59aace50526
João Louzada <joaotande@yahoo.com.br>
2014-11-15 03:19:09 -0200

    HackBot V1.0
    HackBot V1.0

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,21 @@
 /*
 João Ricardo Louzada
 joaotande@yahoo.com.br
-Kansas City - MO, UMKC 2014
-Aqui é Brasil PORRA! heuaheuhua
+Itajubá - MG, UNIFEI 2014
+Aqui é Brasil PORRA!
 */
+//
+//HACKBOT - Project
+//
 //ABOUT THE CODE.
-//This code was constructed with different pieces of others codes that I found in the internet.
-//It is a example about how read and write in space of memory from another process.
-//In this example I am reading a space of memory from notepad and I can change what is written there.
-//The adress that I used is a dinamic adress. So you need use the CheatEngine to find another valid adress to make this code work.
-//I tried find a pointer to do not need use the CheatEngine all the times that I want to use this code. But I did not find it =/.
-//This Code was modified from the original one because that was not working in my X64 windows 8.1. So if you want to use my code you must compile it as a X64 code.
+//This code is a hack for the game Nidhogg. With this code is possible
+//Construct bots in order to control the avatars in the game.
+//
+//Version 1.0.
+//
+//In this version you can know the avatar's positions and control the
+//movement of them.
+
 
 
 #include <iostream>
@@ -20,6 +25,8 @@ Aqui é Brasil PORRA! heuaheuhua
 #include <windows.h>
 #include <iostream>
 #include <string.h>
+#include <iostream>      
+#include <thread>         
 using namespace std;
 
 DWORD GetProcessBaseAddress(HWND window, DWORD pID);
@@ -27,20 +34,27 @@ int apertaG();
 int apertaA();
 int apertaF();
 int apertaD();
+int apertaM();
+int apertaN();
+int apertaRightNarrow();
+int apertaLeftNarrow();
 DWORD getAndressPositionXYellow(HWND window, DWORD pID, HANDLE phandle);
+DWORD getAndressPositionXOrange(HWND hwnd, DWORD pid, HANDLE phandle);
+void playerOne(HWND hwnd, DWORD pid, HANDLE phandle);
+void playerTwo(HWND hwnd, DWORD pid, HANDLE phandle);
+DWORD setPlayerOnePosition(HWND hwnd, DWORD pid, HANDLE phandle, DWORD value);
+DWORD getPlayerOnePosition(HWND hwnd, DWORD pid, HANDLE phandle);
+DWORD setPlayerTwoPosition(HWND hwnd, DWORD pid, HANDLE phandle, DWORD value);
+DWORD getPlayerTwofPosition(HWND hwnd, DWORD pid, HANDLE phandle);
 
 
 
 int main()
 {
-	
-	DWORD address;// = 0x055a6120;//0696B178;//0EA0BEA8A0;// Here you need put a andress of memory that you wanna read or write.
-	DWORD InicialAndress = 0x002B0BD0;
-	DWORD value = 0x1145158528;
 	DWORD pid;
 	DWORD baseAndress;
 	HWND hwnd;
-	//cout << hex << address << "\n";
+	
 	hwnd = FindWindow(NULL, "Nidhogg");// You need put here the title of the main windows of the process that you wanna read and write on.
 	if (!hwnd)
 	{
@@ -59,91 +73,140 @@ int main()
 		}
 		else
 		{
-			while (1)
-			{
+			thread first(playerOne, hwnd, pid, phandle);//Starts Player One Thread
+			thread second(playerTwo, hwnd, pid, phandle);//Starts Player Two Thread
+			cout << "It is working!" << endl;
+			while (1){}
 				
-				cout << "It is working!" << endl;
-				/*ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);//Read a specific position from memory.
-				cout << hex << value << "\n";
-				Sleep(1000);
-				value++;
-				if (WriteProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0))//Write in a specific position of memory.
-				{
-					cout << "It is working!" << endl;
-				}
-				else
-				{
-					cout << "It is not working" << GetLastError() << endl;//
-				}*/
-				//PostMessage(hwnd, WM_ACTIVATE/*WM_CLOSE*/, 'D', 0);// this line just send a message to windows message queu. So you can simulate the press of any button or any another other thing. Try use the spy of Visual studio to get what mesage you wanna use.
-				
-				
-				apertaA();
-				address = getAndressPositionXYellow(hwnd, pid, phandle);
-				ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
-				if (value < 1146044416){
-					value = 1149804544;
-					WriteProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
-				}
-
-				if (value > 1149804544){
-					value = 1146044416;
-					WriteProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
-				}
-				apertaG();
-				apertaF();
-				apertaD();
-				
-				
-				//Find the variable andress from a pointer##########
-				address = getAndressPositionXYellow(hwnd, pid, phandle);
-				ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
-				if (value < 1146044416){
-					value = 1149804544;
-					WriteProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
-				}
-
-				if (value > 1149804544){
-					value = 1146044416;
-					WriteProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
-				}
-				
-				apertaG();
-				apertaF();
-				
-				//printf("Posicao X %d\n\n", value);
-				//Sleep(1000);
-				//###################################################
-				
-				
-				//system("Pause");
-
-			}
 			return 0;
 		}
 	}
 }
 
+DWORD getPlayerOnePosition(HWND hwnd, DWORD pid, HANDLE phandle){
+	DWORD address;
+	DWORD value;
+	
+	address = getAndressPositionXYellow(hwnd, pid, phandle);
+	ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
+	return value;
+}
+
+DWORD setPlayerOnePosition(HWND hwnd, DWORD pid, HANDLE phandle, DWORD value){
+	DWORD address;
+	
+	address = getAndressPositionXYellow(hwnd, pid, phandle);
+	WriteProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
+	return value;
+}
+
+DWORD getPlayerTwoPosition(HWND hwnd, DWORD pid, HANDLE phandle){
+	DWORD address;
+	DWORD value;
+
+	address = getAndressPositionXOrange(hwnd, pid, phandle);
+	ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
+	return value;
+}
+
+DWORD setPlayerTwoPosition(HWND hwnd, DWORD pid, HANDLE phandle, DWORD value){
+	DWORD address;
+
+	address = getAndressPositionXOrange(hwnd, pid, phandle);
+	WriteProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
+	return value;
+}
+
+void playerOne(HWND hwnd, DWORD pid, HANDLE phandle){//Control averything about player ONE
+	DWORD value;
+	
+	while (1){
+		apertaA();
+		/*
+		if (getPlayerOnePosition(hwnd, pid, phandle) < 1146044416){
+			value = 1149804544;
+			setPlayerOnePosition(hwnd, pid, phandle, value);
+		}
+		if (getPlayerOnePosition(hwnd, pid, phandle) > 1149804544){
+			value = 1146044416;
+			setPlayerOnePosition(hwnd, pid, phandle, value);
+		}*/
+		apertaG();
+		apertaF();
+		apertaD();
+		/*
+		if (getPlayerOnePosition(hwnd, pid, phandle) < 1146044416){
+			value = 1149804544;
+			setPlayerOnePosition(hwnd, pid, phandle, value);
+		}
+		if (getPlayerOnePosition(hwnd, pid, phandle) > 1149804544){
+			value = 1146044416;
+			setPlayerOnePosition(hwnd, pid, phandle, value);
+		}
+		*/
+		apertaG();
+		apertaF();
+	}
+}
+
+void playerTwo(HWND hwnd, DWORD pid, HANDLE phandle){//Control everything about player TWO.
+	DWORD value;
+	
+	while (1){
+		apertaM();
+		apertaN();
+		apertaRightNarrow();
+		apertaLeftNarrow();
+	}
+}
+
+
+DWORD getAndressPositionXOrange(HWND hwnd, DWORD pid, HANDLE phandle){
+	DWORD address;
+	DWORD InicialAndress = 0x002C608C;
+	DWORD value;
+	DWORD baseAndress;
+
+	baseAndress = GetProcessBaseAddress(hwnd, pid);
+	address = InicialAndress + baseAndress;
+	ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
+	address = value + 0x0;
+	ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
+	address = value + 0x548;
+	ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
+	address = value + 0xc;
+	ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
+	address = value + 0xb0;
+	ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
+	address = value + 0x8;
+	ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
+	address = value + 0x50;
+
+	return address;
+}
+
 DWORD getAndressPositionXYellow(HWND hwnd, DWORD pid, HANDLE phandle){
 	DWORD address;
-	DWORD InicialAndress = 0x002B0BD0;
-	DWORD value = 0x1145158528;
+	DWORD InicialAndress = 0x002C608C;
+	DWORD value;
 	DWORD baseAndress;
 	
 	baseAndress = GetProcessBaseAddress(hwnd, pid);
 	address = InicialAndress + baseAndress;
 	ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
-	address = value + 0x3f0;
-	ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
 	address = value + 0x0;
 	ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
-	address = value + 0x0;
+	address = value + 0x550;
+	ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
+	address = value + 0xc;
+	ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
+	address = value + 0xb0;
 	ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
 	address = value + 0x8;
 	ReadProcessMemory(phandle, (LPVOID)address, &value, sizeof(value), 0);
-	value = value + 0x50;
-
-	return value;
+	address = value + 0x50;
+	
+	return address;
 }
 
 DWORD GetProcessBaseAddress(HWND window, DWORD pID)
@@ -153,10 +216,12 @@ DWORD GetProcessBaseAddress(HWND window, DWORD pID)
 	module.dwSize = sizeof(MODULEENTRY32);
 	Module32First(snapshot, &module);
 	DWORD base = (DWORD)module.modBaseAddr;
+	//################################################## Show the process list
 	//do {// If uncomented show alll base andress.
 	//	printf("ModuleName: %S\n", module.szExePath);
 	//printf("Base Addr : 0x%08X\n\n", module.modBaseAddr);
 	//} while (Module32Next(snapshot, &module));
+	//##################################################
 	CloseHandle(snapshot);
 	return base;
 }
@@ -181,13 +246,43 @@ int apertaA()
 	return 0;
 }
 
+int apertaLeftNarrow()
+// ver site http://www.codeproject.com/Articles/7305/Keyboard-Events-Simulation-using-keybd-event-funct
+{
+	keybd_event(VK_LEFT, 0x9e, 0, 0); // ‘A’ Press
+	Sleep(1000);
+	keybd_event(VK_LEFT, 0x9e, KEYEVENTF_KEYUP, 0); // ‘A’ Release
+	Sleep(10);
+	return 0;
+}
+
+int apertaRightNarrow()
+// ver site http://www.codeproject.com/Articles/7305/Keyboard-Events-Simulation-using-keybd-event-funct
+{
+	keybd_event(VK_RIGHT, 0x9e, 0, 0); // ‘A’ Press
+	Sleep(1000);
+	keybd_event(VK_RIGHT, 0x9e, KEYEVENTF_KEYUP, 0); // ‘A’ Release
+	Sleep(10);
+	return 0;
+}
+
 
 int apertaF()
 // ver site http://www.codeproject.com/Articles/7305/Keyboard-Events-Simulation-using-keybd-event-funct
 {
 	keybd_event(VkKeyScan('F'), 0xa1, 0, 0); // ‘A’ Press
 	Sleep(100);
-	keybd_event(VkKeyScan('F'), 0xa1, KEYEVENTF_KEYUP, 0); // ‘A’ Release
+	keybd_event(VkKeyScan('F'), 0xa1, KEYEVENTF_KEYUP, 0); // ‘Angfdmgnfamgnfdgmfangfmdngfam’ Release
+	Sleep(10);
+	return 0;
+}
+
+int apertaN()
+// ver site http://www.codeproject.com/Articles/7305/Keyboard-Events-Simulation-using-keybd-event-funct
+{
+	keybd_event(VkKeyScan('N'), 0xb1, 0, 0); // ‘A’ Press
+	Sleep(1000);
+	keybd_event(VkKeyScan('N'), 0xb1, KEYEVENTF_KEYUP, 0); // ‘A’ Release
 	Sleep(10);
 	return 0;
 }
@@ -202,6 +297,15 @@ int apertaD()
 	return 0;
 }
 
+int apertaM()
+//see web site http://www.codeproject.com/Articles/7305/Keyboard-Events-Simulation-using-keybd-event-funct
+{
+	keybd_event(VkKeyScan('M'), 0xb2, 0, 0); // ‘M’ Press
+	Sleep(600);
+	keybd_event(VkKeyScan('M'), 0xb2, KEYEVENTF_KEYUP, 0); // ‘M’ Release
+	Sleep(10);
+	return 0;
+}
 // Infomations extract from http://reverseengineering.stackexchange.com/questions/3482/does-code-injected-into-process-memory-always-belong-to-a-page-with-rwx-access
 /*The assumption does not hold true, as it is possible to alter page protection flags after you've allocated memory.
 The usual mechanism for code injection on Windows is as follows:
@@ -224,3 +328,5 @@ oldProtect = 0;
 VirtualProtectEx(handle, targetAddr, 4096, PAGE_EXECUTE_READ, &oldProtect);
 threadId = 0;
 CreateRemoteThread(handle, NULL, 0, targetAddr, NULL, 0, &threadId);*/
+
+
